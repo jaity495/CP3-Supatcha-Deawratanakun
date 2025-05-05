963441c16acaff52dd9357143c64307d9f2cb516
jaity495 <aung23250@gmail.com>
2025-05-05 15:15:40 +0700

    EX.4

---
+++ assignments/Exercise4_Supatcha_D.py
@@ -0,0 +1,9 @@
+FD = 74
+GB = 67
+ICS = 70
+CP =85
+print("--- Your Score ---")
+print("Foundation English : ",FD )
+print("General Business :",GB )
+print("Introduction to Computer Systems :",ICS )
+print("Computer Programming :",CP )
